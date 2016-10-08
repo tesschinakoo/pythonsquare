aaf37fd4547ca80a8b37145b537ce57291159a0a
tesskoo <tkoo@chadwickschool.org>
2016-10-08 14:11:44 -0700

    add waits before each turn and changed code so that it SHOULD work when robo works (all turns are same angle, so square)

--- Square.py
+++ Square.py
@@ -1,11 +1,14 @@
 # THIS SHOULD ALWAYS BE YOUR FIRST LINE
 from Myro import *
-#init("sim") #if your simulator is not running
+init("sim") #init("sim") #if your simulator is not running
 penDown()
 forward(2,2)
+wait(.5)
 turnLeft(2.48,1)
 forward(2,2)
+wait(.5)
 turnLeft(2.48,1)
 forward(2,1.9)
-turnLeft(2.49,1)
+wait(.5)
+turnLeft(2.48,1)
 forward(2,2)
