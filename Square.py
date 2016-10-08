07ca60cec16231347d7cc16949e4a45b6363eb03
tesskoo <tkoo@chadwickschool.org>
2016-10-08 14:24:21 -0700

    add waits after each turn

--- Square.py
+++ Square.py
@@ -1,14 +1,17 @@
 # THIS SHOULD ALWAYS BE YOUR FIRST LINE
 from Myro import *
-#init("sim") #if your simulator is not running
+init("sim") #if your simulator is not running
 penDown()
 forward(2,2)
 wait(.5)
-turnLeft(2.472,1)
+turnLeft(2.46,1)
+wait(.1)
 forward(2,2)
 wait(.5)
-turnLeft(2.472,1)
-forward(2,1.9)
+turnLeft(2.47,1)
+wait(.1)
+forward(2,2)
 wait(.5)
-turnLeft(2.472,1)
+turnLeft(2.47,1)
+wait(.1)
 forward(2,2)
