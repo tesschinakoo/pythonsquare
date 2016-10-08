7880040f6a49da4a08fc470ff655d5c2ef093786
tesskoo <tkoo@chadwickschool.org>
2016-10-08 14:02:56 -0700

    add commands to draw a square--still inconsistent

--- Square.py
+++ Square.py
@@ -2,4 +2,10 @@
 from Myro import *
 #init("sim") #if your simulator is not running
 penDown()
-forward(5,1)
+forward(2,2)
+turnLeft(2.48,1)
+forward(2,2)
+turnLeft(2.48,1)
+forward(2,1.9)
+turnLeft(2.49,1)
+forward(2,2)
