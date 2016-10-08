95576868a416fc58a16624dcc79ac1321d898a57
tesskoo <tkoo@chadwickschool.org>
2016-10-08 14:17:34 -0700

    change turnLeft speed so that it makes more of a right angle (when it works)

--- Square.py
+++ Square.py
@@ -1,14 +1,14 @@
 # THIS SHOULD ALWAYS BE YOUR FIRST LINE
 from Myro import *
-init("sim") #init("sim") #if your simulator is not running
+#init("sim") #if your simulator is not running
 penDown()
 forward(2,2)
 wait(.5)
-turnLeft(2.48,1)
+turnLeft(2.472,1)
 forward(2,2)
 wait(.5)
-turnLeft(2.48,1)
+turnLeft(2.472,1)
 forward(2,1.9)
 wait(.5)
-turnLeft(2.48,1)
+turnLeft(2.472,1)
 forward(2,2)
